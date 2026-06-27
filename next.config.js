af4b0f12bb0aaae43dab6d512a16c02047fe5c77
CardeeZ <mcarter136988@gmail.com>
2026-06-26 19:45:08 -0700

    Add auth route redirects

--- next.config.js
+++ next.config.js
@@ -8,6 +8,40 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/auth/signin',
+        permanent: false,
+      },
+      {
+        source: '/signin',
+        destination: '/auth/signin',
+        permanent: false,
+      },
+      {
+        source: '/sign-in',
+        destination: '/auth/signin',
+        permanent: false,
+      },
+      {
+        source: '/signup',
+        destination: '/auth/register',
+        permanent: false,
+      },
+      {
+        source: '/sign-up',
+        destination: '/auth/register',
+        permanent: false,
+      },
+      {
+        source: '/register',
+        destination: '/auth/register',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
